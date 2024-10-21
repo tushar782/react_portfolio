c15a936600107b1c9fbf7560130c075b36ede52f
Tushar Pimple <99522469+tushar782@users.noreply.github.com>
2024-10-21 09:20:48 +0530

    resume-update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
 import { FaDownload } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import resume from "../assets/projects/CurriculumVitae.pdf"
+import resume from "../assets/projects/tushar.pdf"
 
 const Project = () => {
     const [currentColorIndex, setCurrentColorIndex] = useState(0);
